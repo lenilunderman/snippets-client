365456a8494e32c92387a721789691e2a4457bf0
leni lunderman <lenilunderman@gmail.com>
2020-08-10 13:59:52 -0400

    Before heroku part 1

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://aqueous-brushlands-45402.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
